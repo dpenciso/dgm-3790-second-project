53d5899ea88233f98324c9b9a3cbf4b8414dc70e
David Enciso <dpenciso@msn.com>
2020-10-22 13:45:29 -0600

    changed all components to functional components

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import './header.css'
 
-class Header extends Component {
-    render() {
-        return (
-            <div className="theDiv">
-                <h1>These are all the Amiibos ever released</h1>
-            </div>
-        )
-    }
+function Header() {
+    return (
+        <div className="theDiv">
+            <h1>These are all the Amiibos ever released</h1>
+        </div>
+    )
 }
-
-
-
-
 
 export default Header;

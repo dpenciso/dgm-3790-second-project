a43947707be1a7a3a5e8d123bc9f80dec7490492
David Enciso <dpenciso@msn.com>
2020-10-28 15:32:21 -0600

    fixed paths

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import SignContextProvider from "./Contexts/SignContext";
+import SignContextProvider from "./contexts/signContext";
 
 ReactDOM.render(
   <React.StrictMode>

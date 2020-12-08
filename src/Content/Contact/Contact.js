b00689c7cdff929d87786cfdf4763371824ab6ee
David Enciso <dpenciso@msn.com>
2020-12-08 15:33:13 -0700

    contact form

--- src/Content/Contact/Contact.js
+++ src/Content/Contact/Contact.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 function Contact() {
-    return (
-        <h1>Hello Contact</h1>
-    )
+  return (
+    <div>
+      <h1>Hello Contact</h1>
+      <div className="contact-box">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfbAgfLF8LAj25IN9xFRTvBDoNo-yZEd4QETSWk0M9E9yMIUQ/viewform?embedded=true"
+          width="640"
+          height="800"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
